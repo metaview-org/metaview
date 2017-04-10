5443c3a7c546be56ba2799ec7a93bd05b9d1f94c
Jakub Hlusička <limeth@protonmail.com>
2017-04-10 22:20:26 +0200

    get basic xml output working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::process::Command;
+use std::process::ExitStatus;
+use std::process::Stdio;
+
+pub const NMAP_NAME: &'static str = "nmap";
+
 fn main() {
-    println!("Hello, world!");
+    let mut command = Command::new(NMAP_NAME);
+
+    command.arg("-oX");
+    command.arg("-");
+
+    for arg in std::env::args() {
+        command.arg(&arg);
+    }
+
+    command.spawn()
+        .unwrap()
+        .wait();
 }
